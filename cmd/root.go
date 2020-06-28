40c3697d79fefec6238f2dfb83716207ab94c644
Poltergeist <spectator.networx@gmail.com>
2020-06-28 10:13:05 +0200

    feat(root): add help description for root command

--- cmd/root.go
+++ cmd/root.go
@@ -35,13 +35,11 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "journalp",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "journalp the personal log for the command line",
+	Long: `journalp is the command line tool to keep a journal on the command line.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+It is using either a enviornment variable or a flag for path discovery.
+Then generating directories and files based on the date.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
